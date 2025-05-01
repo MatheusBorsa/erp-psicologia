c7239e7836758a3ffaba2d5944444640fcdde597
MatheusBorsa <matheusalek485@icloud.com>
2025-05-01 18:49:06 -0300

    feat: base style for login page

---
+++ erp_psicologia_classes/Application/UseCases/Patient/CreatePatientUseCase.cs
@@ -0,0 +1,18 @@
+﻿using erp_psicologia_classes.Application.UseCases.Patient.Dtos;
+using erp_psicologia_classes.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erp_psicologia_classes.Application.UseCases.Patient
+{
+    public class CreatePatientUseCase : IUseCase<CreatePatientInputDto, CreatePatientOutputDto>
+    {
+        public CreatePatientOutputDto Execute(CreatePatientInputDto dto)
+        {
+            return new CreatePatientOutputDto(true);
+        }
+    }
+}
